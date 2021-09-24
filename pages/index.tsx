f55d5e0c749ced73a4020d0c46814c1da7f0c3fa
victorgrodriguesm7 <victorgrodriguesm7@gmail.com>
2021-09-24 20:37:23 -0300

    Add: new Home Styles and Header

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,15 +2,19 @@ import type { GetStaticPropsResult, GetStaticProps, NextPage } from 'next'
 import type { Topic } from '../services/api_interfaces';
 
 import Api from '../services/api';
+
+import styles from '../styles/pages/Home.module.css';
+import Header from '../components/Header';
 interface HomeProps {
   topics: Array<Topic>;
 }
 
 const Home: NextPage<HomeProps> = (props: HomeProps) => {
   const { topics } = props;
+
   return (
-    <div>
-      Hello World
+    <div className={styles.container}>
+      <Header/>
     </div>
   )
 }
