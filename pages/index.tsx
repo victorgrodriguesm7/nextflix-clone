06c8217f0f2db4b83d5011bf4fb76ac76d43e87a
victorgrodriguesm7 <victorgrodriguesm7@gmail.com>
2021-09-24 20:36:58 -0300

    Add: Movies and Series first Loading

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,13 @@
-import type { NextPage } from 'next'
+import type { GetStaticPropsResult, GetStaticProps, NextPage } from 'next'
+import type { Topic } from '../services/api_interfaces';
 
-const Home: NextPage = () => {
+import Api from '../services/api';
+interface HomeProps {
+  topics: Array<Topic>;
+}
+
+const Home: NextPage<HomeProps> = (props: HomeProps) => {
+  const { topics } = props;
   return (
     <div>
       Hello World
@@ -9,3 +16,13 @@ const Home: NextPage = () => {
 }
 
 export default Home
+
+export async function getStaticProps(context: GetStaticProps): Promise<GetStaticPropsResult<HomeProps>>{
+  const topics: Array<Topic> = await Api.getHomeList();
+
+  return {
+    props: {
+      topics: topics
+    }
+  }
+}
